598a36a2593a6431d0d2a3a6f1c2469b9890d69a
Manuel Vilche <manuelvilche@gmail.com>
2021-03-09 01:03:30 -0300

    fix web.js

--- web.js
+++ web.js
@@ -6,12 +6,11 @@ const packageInfo = require('./package.json');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ version: packageInfo.version });
+	res.json({ version: packageInfo.version });
 });
 
 const server = app.listen(process.env.PORT, () => {
-	const host = server.address().address;
-	const port = server.address().port;
+	const { port, address: host } = server.address().port;
 
 	console.log('Web server started at http://%s:%s', host, port);
 });
